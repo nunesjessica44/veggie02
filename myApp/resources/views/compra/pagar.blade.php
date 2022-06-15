4a24bdb7ca83077d4c75a553f96bae26fba7efbd
gringx <brunoferreira1256@outlook.com>
2022-06-14 22:15:22 -0300

    Recupera a bandeira do cartão

--- myApp/resources/views/compra/pagar.blade.php
+++ myApp/resources/views/compra/pagar.blade.php
@@ -19,13 +19,33 @@
             }
             var hash = response.senderHash
             $(".hashseller").val(hash)
-
            })
+
+           let ncartao = $(this).val()
+           $(".bandeira").val("")
+           if(ncartao.length > 6){
+                //Recupera os 6 primeiros dígitos p/ saber a bandeira
+                let prefixcartao = ncartao.substr(0, 6)
+                PagSeguroDirectPayment.getBrand({
+                    cardBin : prefixcartao,
+                    success : function(response){
+                        $(".bandeira").val(response.brand.name)
+                    },
+                    error : function(response){
+                        alert("Número do cartão inválido")
+                    }
+                })
+           }
+
        })
 
        $(".nparcela").on('blur', function(){
-            var bandeira = 'visa';
+            var bandeira = $(".bandeira").val();
             var totalParcelas = $(this).val();
+            if(bandeira == ""){
+                alert("Preencha o número do cartão válido")
+                return ;
+            }
 
             PagSeguroDirectPayment.getInstallments({
                 amount : $(".totalfinal").val(),
@@ -78,6 +98,8 @@
   </table>
 @endif
         <input type="text" name="hashseller" class="hashseller">
+        <input type="text" name="bandeira" class="bandeira">
+
         <div class="row">
             <div class="col-4">
                 Cartão de Crédito:
