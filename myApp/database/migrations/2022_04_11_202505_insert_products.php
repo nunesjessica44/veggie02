7a0d327029b323cdac1d207570701b7ce25aa8b3
salazar-dev <salazar.telecom@gmail.com>
2022-06-21 22:26:38 -0300

    Atualização preços

--- myApp/database/migrations/2022_04_11_202505_insert_products.php
+++ myApp/database/migrations/2022_04_11_202505_insert_products.php
@@ -26,37 +26,37 @@ return new class extends Migration
         $cat = new \App\Models\Categoria(['categoria' => 'óleos' ]);
         $cat->save();
 
-        $prod = new \App\Models\Produto(['nome' => 'Cacal', 'valor' => 10, 'foto' => 'images/produto1-cacal.jpg', 'descricao' =>'', 'categoria_id' => 3]);
+        $prod = new \App\Models\Produto(['nome' => 'Cacau', 'valor' => 29.50, 'foto' => 'images/produto1-cacal.jpg', 'descricao' =>'', 'categoria_id' => 3]);
         $prod->save();
 
-        $prod2 = new \App\Models\Produto(['nome' => 'Suco de Maça', 'valor' => 10, 'foto' => 'images/produto2-sucoMaca.jpg', 'descricao' =>'', 'categoria_id' => 2]);
+        $prod2 = new \App\Models\Produto(['nome' => 'Suco de Maçã', 'valor' => 10.90, 'foto' => 'images/produto2-sucoMaca.jpg', 'descricao' =>'', 'categoria_id' => 2]);
         $prod2->save();
 
-        $prod3 = new \App\Models\Produto(['nome' => 'Açucar de Coco', 'valor' => 10, 'foto' => 'images/produto3-acucarCoco.jpg', 'descricao' =>'', 'categoria_id' => 1]);
+        $prod3 = new \App\Models\Produto(['nome' => 'Açucar de Coco', 'valor' => 14.32, 'foto' => 'images/produto3-acucarCoco.jpg', 'descricao' =>'', 'categoria_id' => 1]);
         $prod3->save();
 
-        $prod4 = new \App\Models\Produto(['nome' => 'Farinha de Coco', 'valor' => 10, 'foto' => 'images/produto4-farinhaCoco.jpg', 'descricao' =>'', 'categoria_id' => 3]);
+        $prod4 = new \App\Models\Produto(['nome' => 'Farinha de Coco', 'valor' => 24.99, 'foto' => 'images/produto4-farinhaCoco.jpg', 'descricao' =>'', 'categoria_id' => 3]);
         $prod4->save();
 
-        $prod5 = new \App\Models\Produto(['nome' => 'Farinha de Amêndoa', 'valor' => 10, 'foto' => 'images/produto5-farinhaAmendoa.jpg', 'descricao' =>'', 'categoria_id' => 3]);
+        $prod5 = new \App\Models\Produto(['nome' => 'Farinha de Amêndoa', 'valor' => 77.60, 'foto' => 'images/produto5-farinhaAmendoa.jpg', 'descricao' =>'', 'categoria_id' => 3]);
         $prod5->save();
 
-        $prod6 = new \App\Models\Produto(['nome' => 'Azeitona', 'valor' => 10, 'foto' => 'images/produto6-azeitona.jpg', 'descricao' =>'', 'categoria_id' => 1]);
+        $prod6 = new \App\Models\Produto(['nome' => 'Azeitona', 'valor' => 29.98, 'foto' => 'images/produto6-azeitona.jpg', 'descricao' =>'', 'categoria_id' => 1]);
         $prod6->save();
 
-        $prod7 = new \App\Models\Produto(['nome' => 'Azeite de Oliva', 'valor' => 10, 'foto' => 'images/produto7-azeiteOliva.jpg', 'descricao' =>'', 'categoria_id' => 4]);
+        $prod7 = new \App\Models\Produto(['nome' => 'Azeite de Oliva', 'valor' => 23.00, 'foto' => 'images/produto7-azeiteOliva.jpg', 'descricao' =>'', 'categoria_id' => 4]);
         $prod7->save();
 
-        $prod8 = new \App\Models\Produto(['nome' => 'Nozes', 'valor' => 10, 'foto' => 'images/produto8-nozes.jpg', 'descricao' =>'', 'categoria_id' => 1]);
+        $prod8 = new \App\Models\Produto(['nome' => 'Nozes', 'valor' => 53.25, 'foto' => 'images/produto8-nozes.jpg', 'descricao' =>'', 'categoria_id' => 1]);
         $prod8->save();
 
-        $prod9 = new \App\Models\Produto(['nome' => 'Proteina de Soja', 'valor' => 10, 'foto' => 'images/produto9-proteinaSoja.jpg', 'descricao' =>'', 'categoria_id' =>1]);
+        $prod9 = new \App\Models\Produto(['nome' => 'Proteína de Soja', 'valor' => 12.00, 'foto' => 'images/produto9-proteinaSoja.jpg', 'descricao' =>'', 'categoria_id' =>1]);
         $prod9->save();
 
-        $prod10 = new \App\Models\Produto(['nome' => 'Proteina de Soja', 'valor' => 10, 'foto' => 'images/produto10-proteinaSoja2.jfif', 'descricao' =>'', 'categoria_id' => 1]);
+        $prod10 = new \App\Models\Produto(['nome' => 'Proteína de Soja', 'valor' => 5.03, 'foto' => 'images/produto10-proteinaSoja2.jfif', 'descricao' =>'', 'categoria_id' => 1]);
         $prod10->save();
 
-        $prod11 = new \App\Models\Produto(['nome' => 'Suco de Uva', 'valor' => 10, 'foto' => 'images/produto11-sucoUva.jpg', 'descricao' =>'', 'categoria_id' => 2]);
+        $prod11 = new \App\Models\Produto(['nome' => 'Suco de Uva', 'valor' => 12.40, 'foto' => 'images/produto11-sucoUva.jpg', 'descricao' =>'', 'categoria_id' => 2]);
         $prod11->save();
         
     }
